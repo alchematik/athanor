635e414a27d27f63a38bf9e0a3fa7c7932feb5ef
Kohei Hisakuni <khisakuni@gmail.com>
2024-02-11 11:00:01 -0800

    Delete old commands

--- internal/evaluator/component.go
+++ internal/evaluator/component.go
@@ -8,18 +8,6 @@ import (
 	"github.com/alchematik/athanor/internal/state"
 )
 
-func (e Evaluator) Component(ctx context.Context, env state.Environment, alias string, comp spec.Component) error {
-	switch c := comp.(type) {
-	case spec.ComponentResource:
-		_, err := e.resource(ctx, env, alias, c)
-		return err
-	case spec.ComponentBuild:
-		return e.build(ctx, env, alias, c)
-	default:
-		return fmt.Errorf("unknown component type: %T", comp)
-	}
-}
-
 func (e Evaluator) resource(ctx context.Context, env state.Environment, alias string, comp spec.ComponentResource) (state.Resource, error) {
 	val, err := e.Value(ctx, env, comp.Value)
 	if err != nil {
@@ -31,17 +19,5 @@ func (e Evaluator) resource(ctx context.Context, env state.Environment, alias st
 		return state.Resource{}, fmt.Errorf("expected Resource type, got %T", val)
 	}
 
-	// env.States[alias] = r
 	return r, nil
-}
-
-func (e Evaluator) build(ctx context.Context, env state.Environment, alias string, comp spec.ComponentBuild) error {
-	subEnv, err := e.Evaluate(ctx, comp.Spec)
-	if err != nil {
-		return err
-	}
-
-	env.States[alias] = subEnv
-
-	return nil
 }
